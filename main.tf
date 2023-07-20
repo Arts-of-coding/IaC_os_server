30e89d9f3dd1ab7474c0bb608ef386c175a1714a
Arts-of-coding <julian.armando.arts@gmail.com>
2023-07-20 16:10:25 +0200

    mamba for speed and first test

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ provider "null" {}
 
 resource "null_resource" "create_conda_environment_1" {
   provisioner "local-exec" {
-    command = "conda create --name my_environment python=3.9 -y"
+    command = "mamba create --name my_environment python=3.9 -y"
   }
 }
 
 resource "null_resource" "create_conda_environment_yml1" {
   provisioner "local-exec" {
-    command = "conda env create -f conda_yml/jupyter.yml"
+    command = "mamba env create -f conda_yml/jupyter.yml"
   }
 }
---
+++ test_yml/test_jupyter.py
@@ -0,0 +1,6 @@
+import seaborn as sns
+import scanpy as sc
+import pandas as pd
+import leidenalg
+
+print("test succesfull!")
