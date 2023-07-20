7a9a390a5beb108497052de1d007abc2f6c9c3b0
Arts-of-coding <julian.armando.arts@gmail.com>
2023-07-20 14:50:18 +0200

    test interpreter try1

--- main.tf
+++ main.tf
@@ -2,21 +2,21 @@ provider "null" {}
 
 resource "null_resource" "create_conda_environment" {
   provisioner "local-exec" {
-    command = "conda create --name my_environment python=3.9 -y"
-    interpreter = ["/bin/bash","-Command"]
+    command = "create --name my_environment python=3.9 -y"
+    interpreter = ["conda"]
   }
 }
 
 resource "null_resource" "install_packages" {
   provisioner "local-exec" {
     command = "conda activate my_environment && conda install numpy pandas matplotlib -y && conda run -n my_environment pip install requests"
-    interpreter = ["/bin/bash","-Command"]
+    interpreter = ["/bin/bash"]
   }
 }
 
 resource "null_resource" "export_environment" {
   provisioner "local-exec" {
     command = "conda env export --name my_environment > environment.yml"
-    interpreter = ["/bin/bash","-Command"]
+    interpreter = ["/bin/bash"]
   }
 }
