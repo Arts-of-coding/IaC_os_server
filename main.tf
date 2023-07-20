d44284ee4a007cd3c9c49e5205fb9ed753b0ab99
Arts-of-coding <julian.armando.arts@gmail.com>
2023-07-20 15:20:33 +0200

    try 6

--- main.tf
+++ main.tf
@@ -8,14 +8,12 @@ resource "null_resource" "create_conda_environment" {
 
 resource "null_resource" "install_packages" {
   provisioner "local-exec" {
-    command = "conda activate my_environment && conda install numpy pandas matplotlib -y && conda run -n my_environment pip install requests"
-    interpreter = ["/bin/bash"]
+    command = "conda activate my_environment && conda install numpy pandas matplotlib -y"
   }
 }
 
 resource "null_resource" "export_environment" {
   provisioner "local-exec" {
-    command = "conda env export --name my_environment > environment.yml"
-    interpreter = ["/bin/bash"]
+    command = "conda deactivate"
   }
 }
